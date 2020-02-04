ff86025567b4c7dd5a6ce29ad88c7d7f516f5182
Alex Zhang <zhangbor@cs.washington.edu>
2020-02-03 21:35:56 -0800

    First Day YAYAY

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-d3.csv("../Meteorite_Landings.csv").then(function(data) {
-    console.log(data);
+const d3 = require('d3');
+
+const csvFile = require('./Meteorite_Landings.csv');
+
+/*var name = [];
+var id = [];
+var recclass = [];
+var mass = [];
+var year = [];
+var GeoLocation = [];*/
+
+d3.csv(csvFile, function(data) {
+    return {
+        name: data.name,
+        id: data.id,
+        recclass: data.recclass,
+        mass: +data["mass (g)"],
+        year: data.year,
+        GeoLocation: data.GeoLocation
+    };
+}).then(function(d) {
+    console.log(d);
 });
