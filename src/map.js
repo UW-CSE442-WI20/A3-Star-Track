453facf8aaf0a1294fd94c0e078230b2a302c093
Yongzhi Li <yongzhili@Yongzhis-MacBook-Pro.local>
2020-02-04 20:57:43 -0800

    by angie

---
+++ src/map.js
@@ -0,0 +1,29 @@
+var mymap = L.map('mapid').setView([40, -0.09], 2);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5naWVsZWV5eiIsImEiOiJjazY4c3MwOHAwOGc1M29xanNrOWdpcjgwIn0.kOc4Y88p-f10kvKPyKoKOA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'yellow',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 2000
+}).addTo(mymap);
+
+circle.bindPopup("I am a circle.");
+
+// var popup = L.popup();
+
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(mymap);
+// }
+
+// mymap.on('click', onMapClick);
+
