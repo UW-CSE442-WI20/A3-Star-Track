bc63a787699c97a4b9c262f0ffd9fad662d146fc
Yifan Bai <yfbai@uw.edu>
2020-02-04 22:19:01 -0800

    add year parsing and filtering

--- src/index.js
+++ src/index.js
@@ -8,16 +8,20 @@ var recclass = [];
 var mass = [];
 var year = [];
 var GeoLocation = [];*/
-
+var year = 2010;
 d3.csv(csvFile, function(data) {
     return {
         name: data.name,
         id: data.id,
         recclass: data.recclass,
         mass: +data["mass (g)"],
-        year: data.year,
+        year: parseInt(data.year.substring(6, 10)),
         GeoLocation: data.GeoLocation
     };
 }).then(function(d) {
-    console.log(d);
+    // code to filter the data using year
+    var fd = d.filter(function(row) {
+        return row["year"] == year;
+    });
+    console.log(fd);
 });
