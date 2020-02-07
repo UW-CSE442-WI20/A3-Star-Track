e2e89313c707d0bd408a3719a3e8ad78d42543af
Yifan Bai <yfbai@uw.edu>
2020-02-06 21:32:02 -0800

    year selection, styles, css and parsing data

--- src/index.js
+++ src/index.js
@@ -8,8 +8,26 @@ var recclass = [];
 var mass = [];
 var year = [];
 var GeoLocation = [];*/
-var year = 2010;
-d3.csv(csvFile, function(data) {
+var year = 2000;
+var slider = document.getElementById("yearRange");
+var output = document.getElementById("yearDisplay");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    year = this.value;
+    // TODO: Update the plot here:
+}
+
+function filter_data(d, year, cls) {
+    var d_filtered =  d.filter(function(row) {
+            return row["year"] == year;
+        });
+    return d_filtered;
+}
+
+var all_data = d3.csv(csvFile, function(data) {
     return {
         name: data.name,
         id: data.id,
@@ -18,10 +36,7 @@ d3.csv(csvFile, function(data) {
         year: parseInt(data.year.substring(6, 10)),
         GeoLocation: data.GeoLocation
     };
-}).then(function(d) {
-    // code to filter the data using year
-    var fd = d.filter(function(row) {
-        return row["year"] == year;
-    });
-    console.log(fd);
 });
+console.log(all_data);
+// TODO Resolve this promise!
+// {"year": 2020, "mass": 200, "id":3}
