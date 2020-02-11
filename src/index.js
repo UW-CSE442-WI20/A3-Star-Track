862e97302aa9df833a6006db00ecd4dc773d6772
Alex Zhang <zhangbor@cs.washington.edu>
2020-02-10 21:53:09 -0800

    wo men kuai zuo wan le ha ha ha

--- src/index.js
+++ src/index.js
@@ -15,9 +15,21 @@ var all_data = d3.csv(csvFile, function(data) {
         recclass: data.recclass,
         mass: +data["mass (g)"],
         year: parseInt(data.year.substring(6, 10)),
-        GeoLocation: data.GeoLocation
+        reclat: data.reclat,
+        reclong: data.reclong
     };
 });
+
+var mymap = L.map('mapid').setView([40, -0.09], 2);
+var shipLayer = L.layerGroup();
+mymap.addLayer(shipLayer);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5naWVsZWV5eiIsImEiOiJjazY4c3MwOHAwOGc1M29xanNrOWdpcjgwIn0.kOc4Y88p-f10kvKPyKoKOA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
 
 // returns a Promise of filtered csv array from all_data
 function filter_data() {
@@ -30,28 +42,66 @@ function filter_data() {
     });
 }
 
-// TODO: change it to plotting function...
-function printToConsole() {
+// Legend
+var legend = L.control({ position: "bottomleft" });
+
+legend.onAdd = function(map) {
+  var div = L.DomUtil.create("div", "legend");
+  div.innerHTML += "<h4>Classes</h4>";
+  div.innerHTML += '<i style="background: #30C5FF"></i><span>H Chondrite</span><br>';
+  div.innerHTML += '<i style="background: #E84855"></i><span>L Chondrite</span><br>';
+  div.innerHTML += '<i style="background: #FFFD82"></i><span>Carbonaceous Chondrite</span><br>';
+  div.innerHTML += '<i style="background: #C6D4FF"></i><span>Other</span><br>';
+  
+  return div;
+};
+
+legend.addTo(mymap);
+
+function drawPoints() {
+    shipLayer.clearLayers();
     filter_data().then(function(d) {
         d.map(function(row) {
-            console.log("name: " + row["name"] + ", GeoLocation:" + row["GeoLocation"]);
+            var fill = "";
+            // get color based on class
+            if (row["recclass"].charAt(0) === 'H') {
+                // blue
+                fill = "#30C5FF";
+            } else if (row["recclass"].charAt(0) === 'L') {
+                // red
+                fill = "#E84855";
+            } else if (row["recclass"].charAt(0) === 'C') {
+                // yellow
+                fill = "#FFFD82";
+            } else {
+                // purple
+                fill = "#C6D4FF";
+            }
+
+            var circle = L.circle([row["reclat"], row["reclong"]], {
+                stroke: false,
+                fillColor: fill,
+                fillOpacity: 0.9,
+                radius: Math.min((row["mass"] + 1000.0) * 100.0, 150000)
+            });
+            shipLayer.addLayer(circle);
+            circle.bindPopup("Name: " + row["name"] + "<br>"
+                            + "ID: " + row["id"] + "<br>"
+                            + "Class: " + row["recclass"] + "<br>"
+                            + "Mass (g): " + row["mass"] + "<br>"
+                            + "Geolocation: (" + row["reclat"] + ", " + row["reclong"] + ")");
         });
     })
     
 }
 
-// Print initial data
-printToConsole();
+// Draw points (meteorites) on the map
+drawPoints();
 
 // Update the current slider value and render a new plot
 slider.oninput = function() {
     output.innerHTML = this.value;
     year = this.value;
     // TODO: Update the plot here:
-    printToConsole();
+    drawPoints();
 }
-
-
-
-// TODO Resolve this promise!
-// {"year": 2020, "mass": 200, "id":3}
